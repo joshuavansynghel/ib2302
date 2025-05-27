aeab1c0c5ef09ded675424929cb2e616cb6cc2f0
Joshua Van Synghel <joshua.vansyngel@hotmail.com>
2025-05-27 21:33:19 +0200

    restored week 1 exercises

--- Opdrachten/src/week1/RockPaperScissorsCheatingProcess.java
+++ Opdrachten/src/week1/RockPaperScissorsCheatingProcess.java
@@ -1,19 +1,67 @@
 package week1;
 
+import java.util.Set;
+
 import framework.Channel;
 import framework.IllegalReceiveException;
 import framework.Message;
 import framework.Process;
 
 public class RockPaperScissorsCheatingProcess extends Process {
+	
+	private Set<Channel> incomingChannels = null;
+	
+	private int n;
+	private boolean win = false;
+	private boolean lose = false;
+	private Item move;
 
 	@Override
 	public void init() {
-		// TODO
+		// Initialize instance variables
+		this.n = getOutgoing().size(); // set number of message to send
+		this.incomingChannels = getIncoming(); // channels who still need to send message
 	}
 
 	@Override
 	public void receive(Message m, Channel c) throws IllegalReceiveException {
-		// TODO
+		// if channel not supposed to or already sent message, throw error
+		if (!this.incomingChannels.remove(c)) {
+			throw new IllegalReceiveException();
+		}
+		// if message not of correct class, throw error
+		else if (!(m instanceof RockPaperScissorsMessage)) {
+			throw new IllegalReceiveException();
+		}
+		
+		Item moveOpponent = ((RockPaperScissorsMessage) m).getItem();
+		
+		// choose best move based on first message received
+		if ((moveOpponent == Item.PAPER) && (n == getOutgoing().size())) {
+			this.move = Item.SCISSORS;
+			this.win = true;
+		}
+		else if ((moveOpponent == Item.ROCK) && (n == getOutgoing().size())) {
+			this.move = Item.PAPER;
+			this.win = true;
+		}
+		else if ((moveOpponent == Item.SCISSORS) && (n == getOutgoing().size())) {
+			this.move = Item.ROCK;
+			this.win = true;
+		}
+		
+		this.n -= 1;
+		
+		if (moveOpponent.beats(this.move)) {
+			this.lose = true;
+		}
+
+		// if all message received send message and print result
+		if (this.n == 0) {
+			for (Channel cOut: getOutgoing()) {
+				send(new RockPaperScissorsMessage(this.move), cOut);
+			}
+			print(this.win + " " + this.lose);
+		}
 	}
 }
--- Opdrachten/src/week1/RockPaperScissorsProcess.java
+++ Opdrachten/src/week1/RockPaperScissorsProcess.java
@@ -1,19 +1,61 @@
 package week1;
 
+import java.util.Set;
+
 import framework.Channel;
 import framework.IllegalReceiveException;
 import framework.Message;
 import framework.Process;
 
+
 public class RockPaperScissorsProcess extends Process {
+	
+	private Set<Channel> incomingChannels = null;
+	
+	private int n;
+	private boolean win = false;
+	private boolean lose = false;
+	private Item move;
 	
 	@Override
 	public void init() {
-		// TODO
+		// Initialize instance variables
+		this.n = getOutgoing().size(); // set number of message to receive
+		this.incomingChannels = getIncoming(); // channels who still need to send message
+		this.move = Item.random(); // a random move
+		
+		// Send message through all outgoing channels
+		Message m = new RockPaperScissorsMessage(this.move);
+		for (Channel c: getOutgoing()) {
+			send(m, c);
+		}
 	}
 
 	@Override
 	public void receive(Message m, Channel c) throws IllegalReceiveException {
-		// TODO
+		// if channel not supposed to or already sent message, throw error
+		if (!this.incomingChannels.remove(c)) {
+			throw new IllegalReceiveException();
+		}
+		// if message not of correct class, throw error
+		else if (!(m instanceof RockPaperScissorsMessage)) {
+			throw new IllegalReceiveException();
+		}
+		
+		this.n -= 1;
+		Item moveOpponent = ((RockPaperScissorsMessage) m).getItem();
+		
+		if (this.move.beats(moveOpponent)) {
+			this.win = true;
+		}
+		if (moveOpponent.beats(this.move)) {
+			this.lose = true;
+		}
+
+		// if all message received print result
+		if (this.n == 0) {
+			print(this.win + " " + this.lose);
+		}
+		
 	}
 }
