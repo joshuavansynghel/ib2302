0ba9a683796afe1c0cb47a5683b9a1968b68df98
Joshua Van Synghel <joshua.vansyngel@hotmail.com>
2025-06-09 19:47:49 +0200

    solved DFS

--- Opdrachten/src/week56/DepthFirstSearchInitiator.java
+++ Opdrachten/src/week56/DepthFirstSearchInitiator.java
@@ -5,14 +5,45 @@ import framework.IllegalReceiveException;
 import framework.Message;
 
 public class DepthFirstSearchInitiator extends DepthFirstSearchProcess {
+	
+	private int tokensReceived = 0;
 
 	@Override
 	public void init() {
-		// TODO
+		super.init();
+		send(new TokenMessage(), getRandomOutgoingChannels().get(0));
+		removeNextOutgoingChannel();
 	}
 
 	@Override
 	public void receive(Message m, Channel c) throws IllegalReceiveException {
-		// TODO
+		super.receive(m, c);
+		if (m instanceof TokenMessage) {
+			incrementTokensReceived();
+		}
+		
+		// if all tokens received, finish algorithm
+		if (getTokensReceived() == getOutgoing().size()) {
+			done();
+		}
+		// if rule 1 and 2 allow it, send message back to sender
+		else if ((getRandomOutgoingChannels().size() != 0) &&
+				(getRandomOutgoingChannels().contains(getReversedChannel(c)))){
+			send(m, getReversedChannel(c));
+			removeSpecificOutgoingChannel(getReversedChannel(c));
+			}
+		else {
+			send(m, getRandomOutgoingChannels().get(0));
+			removeNextOutgoingChannel();;
+		}
+		
+	}
+	
+	public int getTokensReceived() {
+		return tokensReceived;
+	}
+	
+	public void incrementTokensReceived() {
+		tokensReceived += 1;
 	}
 }
--- Opdrachten/src/week56/DepthFirstSearchNonInitiator.java
+++ Opdrachten/src/week56/DepthFirstSearchNonInitiator.java
@@ -3,16 +3,59 @@ package week56;
 import framework.Channel;
 import framework.IllegalReceiveException;
 import framework.Message;
+import framework.Process;
 
 public class DepthFirstSearchNonInitiator extends DepthFirstSearchProcess {
+	
+	private Process parent;
 
 	@Override
 	public void init() {
-		// TODO
 	}
 
 	@Override
 	public void receive(Message m, Channel c) throws IllegalReceiveException {
-		// TODO
+		super.receive(m, c);
+		if ((m instanceof TokenMessage) && getParent() == null) {
+			Process p = c.getSender();
+			setParent(p);
+			super.init();
+			removeSpecificOutgoingChannel(getOutgoingToParent(p));
+		}
+		
+		// if rule 1 and 2 allow it, send message back to sender
+		if ((getRandomOutgoingChannels().size() != 0) &&
+				(getRandomOutgoingChannels().contains(getReversedChannel(c)))){
+			send(m, getReversedChannel(c));
+			removeSpecificOutgoingChannel(getReversedChannel(c));
+		}
+		// else send message to next random outgoing channel
+		else if (getRandomOutgoingChannels().size() != 0) {
+			send(m, getRandomOutgoingChannels().get(0));
+			removeNextOutgoingChannel();
+		}
+		// if all messages are sent to outgoing channels, send back to parent
+		else if (isActive()) {
+			send(m, getOutgoingToParent(getParent()));
+			done();
+		}
+	}
+	
+	public Process getParent() {
+		return parent;
+	}
+	
+	public void setParent (Process p) {
+		this.parent = p;
+	}
+	
+	private Channel getOutgoingToParent (Process p) {
+		Channel channelToParent = null; 
+		for (Channel c: getOutgoing()) {
+			if (c.getReceiver() == p) {
+				channelToParent = c;
+			}
+		}
+		return channelToParent;
 	}
 }
