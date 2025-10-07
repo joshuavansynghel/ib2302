b077a4cc914dc6939ba2c5c092ad59e1778549c3
Joshua Van Synghel <joshua.vansyngel@hotmail.com>
2025-10-07 20:54:51 +0200

    add temp progress deadlock detection

--- Opdrachten/src/week78/BrachaTouegInitiator.java
+++ Opdrachten/src/week78/BrachaTouegInitiator.java
@@ -8,11 +8,34 @@ public class BrachaTouegInitiator extends BrachaTouegProcess {
 
 	@Override
 	public void init() {
-		// TODO
+		super.init();
+		
+		sendNotifyToOutRequests();
+		
+		// If initial list is empty, send acks
+		if(super.requests == 0) {
+			sendGrantToInRequests();
+		}
 	}
 
 	@Override
 	public void receive(Message m, Channel c) throws IllegalReceiveException {
-		// TODO
+		super.receive(m,  c);
+		
+		// Initiator sends Done upon receiving Notify
+		if (m instanceof NotifyMessage) {
+			send(new DoneMessage(), getIncomingToOutgoing(c));
+		}
+		
+		System.out.println("\nNumber of outRequest: " + getOutRequests().size());
+		if (getOutRequests().size() == 0) {
+			print(String.valueOf(isDeadlocked()));
+		}
+	}
+	
+	private boolean isDeadlocked () {
+		return requests != 0;
 	}
 }
+
+	
